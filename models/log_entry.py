4a2b854cefe663ce3464c9870f8cb6364c6420b3
Szczypiorrr <wintheer09@gmail.com>
2026-05-31 23:58:25 +0200

    feat: add LogEntry model for log representation

---
+++ models/log_entry.py
@@ -0,0 +1,11 @@
+class LogEntry():
+    def __init__(self, name, status, ip):
+        self.name = name
+        self.status = status
+        self.ip = ip
+
+    def __repr__(self):
+        return f"{self.name} | {self.status} | {self.ip}"
+    
+
+print(LogEntry())
