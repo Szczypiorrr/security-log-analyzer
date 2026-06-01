e1522b664c6b11f91f1257b26b4f724ec38d3587
Szczypiorrr <wintheer09@gmail.com>
2026-06-01 17:47:07 +0200

    feat: add report generator for logs

---
+++ analyzer/report.py
@@ -0,0 +1,16 @@
+def generate_report(logs):
+    with open("reports/report.txt", "w") as f:
+        f.write("========= LOG REPORT =========\n\n")
+
+        f.write(f"Total logs: {len(logs)}\n")
+
+        success = 0
+        fail = 0
+        for log in logs:
+            if log.status == "SUCCESS":
+                success += 1
+            else:
+                fail += 1
+
+        f.write(f"SUCCESS: {success}\n")
+        f.write(f"FAIL: {fail}\n")
